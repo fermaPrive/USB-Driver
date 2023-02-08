0ac602c35c45066c242a798925c5a7808fcb3fee
fermaPrive <fermaPrive@protonmail.com>
2023-02-08 23:17:20 +0100

    feat: Added probe and disconnect template

--- usb-driver.c
+++ usb-driver.c
@@ -4,6 +4,7 @@
 #include <linux/init.h>
 #include <linux/usb.h>
 
+#define DRV_NAME "custom-usb-driver"
 #define VENDOR_NAME "USBest Technology"
 #define VENDOR_ID 0x4146
 #define PRODUCT_ID 0x2251
@@ -18,6 +19,15 @@ MODULE_DEVICE_TABLE(usb, usb_device_table);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("fermaPrive");
 MODULE_DESCRIPTION("Driver for usb device");
+
+static int usb_probe(struct usb_interface *inter, const struct usb_device_id *id) {
+    printk(KERN_INFO "Usb drive (%04X:%04X) plugged in.\n", id->idVendor, id->idProduct);
+    return 0;
+}
+
+static void usb_disconnect(struct usb_interface *inter) {
+    printk(KERN_INFO "USB drive removed\n");
+}
 
 
 static int __init init_mod(void) {
